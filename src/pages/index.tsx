ee234e9e474019e45ecd8ad5e4bc560c1072b914
nathaliaassis <nathaliamassis@gmail.com>
2022-01-07 18:18:57 -0300

    first-commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Dynamic title</title>
+      </Head>
+      <h1>
+        title
+      </h1>
+    </>
+  );
+}
