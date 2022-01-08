fbb9c996c5ce95880282d9376b39784f18096a13
nathaliaassis <nathaliamassis@gmail.com>
2022-01-08 00:58:00 -0300

    create component: signInButton

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(true);
+
+  return isUserLoggedIn ? (
+    <button
+      className={styles.signInButton}
+      type='button'>
+      <FaGithub color='#04d361' />
+      Nathália Assis
+      <FiX
+        className={styles.closeIcon}
+        color='#737380'
+      />
+    </button>
+  ) : (
+    <button
+      className={styles.signInButton}
+      type='button'>
+      <FaGithub color='#eba417' />
+      Sign in with Github
+    </button>
+  );
+}
