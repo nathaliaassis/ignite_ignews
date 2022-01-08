9b65378f821d9db45abf06764a033d1d0acce4d2
nathaliaassis <nathaliamassis@gmail.com>
2022-01-08 12:22:11 -0300

    authentication with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,19 @@
-import { useState } from 'react';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 import styles from './styles.module.scss';
 
 export function SignInButton() {
+  const { data: session } = useSession();
 
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
-
-  return isUserLoggedIn ? (
+  return session ? (
     <button
       className={styles.signInButton}
-      type='button'>
+      type='button'
+      onClick={() => signOut()}
+    >
       <FaGithub color='#04d361' />
-      Nathália Assis
+      {session.user?.name}
       <FiX
         className={styles.closeIcon}
         color='#737380'
@@ -21,7 +22,9 @@ export function SignInButton() {
   ) : (
     <button
       className={styles.signInButton}
-      type='button'>
+      type='button'
+      onClick={() => signIn('github')}
+    >
       <FaGithub color='#eba417' />
       Sign in with Github
     </button>
